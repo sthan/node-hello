939478e30d96e5f666e81d4b33a02d4c7e5e119c
Shubhasish <panda.subhasish007@gmail.com>
2023-02-22 08:52:13 +0530

    test commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node Spinnaker test 1234!\n'
+  const msg = 'Hello Node Spinnaker test 12345!\n'
   res.end(msg);
 });
 
